4154ec58a4e5b7bf06427b3044515990d0e9f248
onokoro0822 <watau0822@gmail.com>
2024-09-22 01:45:24 +0900

    Deploy to GitHub Pages

---
+++ src/About.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import "./About.css";
+
+function About(){
+    return(
+        <div className='About' style={{paddingTop:'60px'}}>
+            <div className='sectionTitle'>
+            About Us
+            </div>
+            <div className='introduction'>
+            美術家・野老朝雄氏が講師を務める東京大学の授業「造形第六」において、制作活動に参加した学生の中から、有志メンバーによる展示が行われることになりました。<br/>
+            造形第六では2014年以降”CONDITION SPECIFIC”という通底するテーマに沿って作品が制作され、建築という枠組みを超えた表現が試みられてきました。<br/>
+            今年度はサブテーマ”MAKING INBETWEEN”を起点としてグラフィックや彫刻、ソフトウェアなど、さまざまなメディアを用いた5つの作品が制作されました。<br/>
+            ここでは、それぞれの制作メンバーをご紹介します。
+            </div>
+
+            <div className='ProjectTeam'>
+                <div className='sectionTitle'>
+                Project Team
+                </div>
+                
+                <div className='teamTitle'>
+                    0と1のあいだ
+                </div>
+                <div className='teamDoing'>
+                    有と無
+                </div>
+                <div className='teamMenber'>
+                    <span>Mai Kawakami</span>
+                    <span>Lisa Kudo</span>
+                    <span>Yuuka Sayama</span>
+                    <span>Mayu Watanabe</span>
+                    <span>Suzuka Shoji</span>
+                </div>
+
+                <div className='teamTitle migi'>
+                    カオスとコスモス
+                </div>
+                <div className='teamDoing migi'>
+                    多様なメディアの調和を探求
+                </div>
+                <div className='teamMenber migi'>
+                    <span>Saki Kanouchi</span>
+                    <span>Ryo Sakai</span>
+                    <span>Sae Fujinaga</span>
+                    <span>Jun Takaura</span>
+                    <span>Taiga Yamaguchi</span>
+                    <span>Yuki Watanabe</span>
+                    <span>Shuji Hisatomi</span>
+                </div>
+
+                <div className='teamTitle'>
+                    偏光板
+                </div>
+                <div className='teamDoing'>
+                    光と影の対立を表現
+                </div>
+                <div className='teamMenber'>
+                    <span>Kenjiro Kawai</span>
+                    <span>Akira Takeuchi</span>
+                    <span>Ikumi Hirai</span>
+                    <span>Tomoya Matsuki</span>
+                    <span>Yuki Matsuhisa</span>
+                    <span>Kai Matsumoto</span>
+                </div>
+
+                <div className='teamTitle migi'>
+                    東京と故郷のあいだ
+                </div>
+                <div className='teamDoing migi'>
+                    都市と故郷をつなぐアート
+                </div>
+                <div className='teamMenber migi'>
+                    <span>Mebuki Kanda</span>
+                    <span>Yuri Sakomoto</span>
+                    <span>Ryo Sakai</span>
+                    <span>Issa Takagi</span>
+                    <span>Akira Takeuchi</span>
+                    <span>Shuji Hisatomi</span>
+                </div>
+
+                <div className='teamTitle'>
+                    日本語と英語のInbetween
+                </div>
+                <div className='teamDoing'>
+                    言語を超えた表現の探求
+                </div>
+                <div className='teamMenber'>
+                    <span>Shunsuke Oshima</span>
+                    <span>Kaisei Ota</span>
+                    <span>Kenjiro Kawai</span>
+                    <span>Lisa Kudo</span>
+                    <span>Suzuka Shoji</span>
+                    <span>Yuri Sakomoto</span>
+                    <span>Eisuke Takahashi</span>
+                    <span>Masato Tominaga</span>
+                    <span>Shuji Hisatomi</span>
+                    <span>Mayu Watanabe</span>
+                </div>
+                </div>
+            
+                
+
+
+            <div className='operationTeam'>
+                <div className='sectionTitle'>
+                Oparation Team
+                </div>
+                
+                <div className='introduction'>
+                本展示の開催にあたり、運営チームを発足いたしました。<br/>
+                ここでは、様々な業務に携わったメンバーをご紹介いたします。
+                </div>
+                
+                <div className='teamTitle'>
+                    Operational Management
+                </div>
+                <div className='teamDoing'>
+                    全体の統括
+                </div>
+                <div className='teamMenber'>
+                    <span>Shuji Hisatomi</span>
+                    <span>Saki Kanouchi</span>
+                    <span>Issa Takagi</span>    
+                </div>
+
+                <div className='teamTitle migi'>
+                    Design
+                </div>
+                <div className='teamDoing migi'>
+                    ポスターなどのデザイン
+                </div>
+                <div className='teamMenber migi'>
+                    <span>Ozan Ishii</span>
+                    <span>Shuji Hisatomi</span>
+                    <span>Yuki Matsuhisa</span>
+                    <span>Kai Matsumoto</span>
+                    <span>Issa Takagi</span>
+                    <span>Mayu Watanabe</span>
+                    <span>Yuuka Sayama</span>
+                    <span>Tomoya Matsuki</span>
+                    <span>Jun Takaura</span>
+                    <span>Ikumi Hirai</span>
+                </div>
+
+                <div className='teamTitle'>
+                    Public Relations
+                </div>
+                <div className='teamDoing'>
+                    SNS,ホームページの運用
+                </div>
+                <div className='teamMenber'>
+                    <span>Ozan Ishii</span>
+                    <span>Kai Matsumoto</span>
+                    <span>Mai Kawakami</span>
+                    <span>Issa Takagi</span>
+                    <span>Saki Kanouchi</span>
+                    <span>Jun Takaura</span>
+                    <span>Yuki Watanabe</span>
+                    <span>Yuri Sakomoto</span>
+                    <span>Ikumi Hirai</span>
+                    <span>Sae Fujinaga</span>
+                    <span>Mebuki Kanda</span>
+                </div>
+
+                <div className='teamTitle migi'>
+                    Accounting
+                </div>
+                <div className='teamDoing migi'>
+                    会計作業
+                </div>
+                <div className='teamMenber migi'>   
+                    <span>Suzuka Shoji</span>
+                    <span>Kaisei Ota</span>
+                    <span>Ryo Sakai</span>
+                </div>
+
+                <div className='teamTitle'>
+                    Venue Configuration
+                </div>
+                <div className='teamDoing'>
+                    会場の動線計画やサイン計画
+                </div>
+                <div className='teamMenber'>
+                    <span>Shuji Hisatomi</span>
+                    <span>Kai Matsumoto</span>
+                    <span>Suzuka Shoji</span>
+                    <span>Kaisei Ota</span>
+                    <span>Mai Kawakami</span>
+                    <span>Eisuke Takahashi</span>
+                    <span>Issa Takagi</span>
+                    <span>Mayu Watanabe</span>
+                    <span>Tomoya Matsuki</span>
+                    <span>Masato Tominaga</span>
+                    <span>Yuri Sakomoto</span>
+                    <span>Mebuki Kanda</span>
+                </div>
+
+                <div className='teamTitle migi'>
+                    External Relations
+                </div>
+                <div className='teamDoing migi'>
+                    外部とのやりとり
+                </div>
+                <div className='teamMenber migi'>
+                    <span>Shuji Hisatomi</span>
+                    <span>Tomoya Matsuki</span>
+                    <span>Saki Kanouchi</span>
+                    <span>Issa Takagi</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
